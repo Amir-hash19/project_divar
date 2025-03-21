3db4281671abc99879e746aadd27670ad34da823
amirhoseinheydari <amirhoseinhydri.1381@gmail.com>
2025-03-21 15:25:10 +0330

    :wrench:

--- divar/account/models.py
+++ divar/account/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
+)
 
-# Create your models here.
+class MyAccountManager(BaseUserManager):
+    def create_user(self, email, username, password=None):
+        if not email:
+            raise ValueError("Users must have an email address!")
+        if not username:
+            raise ValueError("Users must have a username")
+        
+        user = self.model(
+            email = self.normalize_email(email),
+            username=username
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+
+
+    def create_super(self, email, username, password):
+        user = self.create_super(
+            email=self.normalize_email(email),
+            password=password,
+            uername=username,
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+
+def get_default_profile_image():
+    return 'profile/profile_default/profile_image.png'
+
+def get_profile_image_filepath(self, filename):
+    return 'profile/profile_images/' + str(self.pk) + '/profile_image.png'
+
+
+class Account(models.Model):
+    username = models.CharField(max_length=60, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    mobile = PhoneNumberField(unique=True, region="IR")
+    date_joined = models.DateTimeField(auto_now_add=True)
+    profile_image = models.ImageField(max_length=255, upload_to=get_default_profile_image, null=True, blank=True, default=get_profile_image_filepath)
+    hide_email = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ['username', 'mobile']
+    objects = MyAccountManager()
+
+    def __str__(self):
+        return f"{self.username} {self.email}"
--- divar/divar/settings.py
+++ divar/divar/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account.apps.AccountConfig',
 ]
 
 MIDDLEWARE = [
@@ -111,6 +113,17 @@ USE_I18N = True
 
 USE_TZ = True
 
+
+STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media_cdn")
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "media"),
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
