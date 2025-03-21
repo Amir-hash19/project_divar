74275a6a2df40c00449a5170129a9499cf5d3018
amirhoseinheydari <amirhoseinhydri.1381@gmail.com>
2025-03-21 15:41:55 +0330

    :poop:

--- divar/account/admin.py
+++ divar/account/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from account.models import Account
+from django.utils import timezone
 
-# Register your models here.
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "mobile", "date_joined")
+    list_filter = ("username",)
+    search_fields = ("username", "email")
+    fields = (("username", "email"), "mobile", "date_joined")
+    list_per_page = 20
+
+
+    def get_ordering(self, request):
+        if request.user.is_superuser:
+            return ("username", "email")
+        return ("username", )
+    
+
+
+admin.site.register(Account)
